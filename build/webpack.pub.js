69d3eea6a2789fe09b8af067c70fda80688b5662
SoldierAb <ab140140@163.com>
2020-03-06 11:37:20 +0800

    feat: ✨ update pub config

--- build/webpack.pub.js
+++ build/webpack.pub.js
@@ -15,27 +15,6 @@ module.exports = merge(baseConfig, {
     },
     optimization: {
         minimize: false,
-        // splitChunks:{
-        //     chunks:'all',
-        //     cacheGroups:{
-        //         vendors: {
-        //             test: /[\\/]node_modules[\\/]/, 
-        //             priority: -10  
-        //         },
-        //         // elementBase: {
-        //         //     test: (module) => {
-        //         //       return /element-ui/.test(module.context);
-        //         //     }, 
-        //         //     name: "elementBase",
-        //         //     priority: 10,
-        //         //   },
-        //         default: {
-        //             minChunks: 2,
-        //             priority: -20,   
-        //             reuseExistingChunk: true
-        //         }
-        //     },
-        // },
     },
     externals:{
         vue: {
@@ -44,7 +23,5 @@ module.exports = merge(baseConfig, {
             commonjs2: 'vue',
             amd: 'vue'
         },
-        'vue-router': 'VueRouter',
-        'element-ui': 'ELEMENT',
     },
 })
