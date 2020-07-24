f1f745afe75e246084a6463ff2e398e1129b041a
gorgechan <chenguijian@dianchu.com>
2020-07-24 11:40:59 +0800

    chore: 🔧 del node-emoji

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,6 +1,4 @@
 /* eslint-disable max-len */
-const emoji = require('node-emoji');
-
 const env = process.env;
 const ADBLOCK = is(env.ADBLOCK);
 const CI = is(env.CI);
@@ -22,9 +20,7 @@ if (!ADBLOCK && !CI && !DISABLE_OPENCOLLECTIVE && !SILENT) {
     '\u001B[96mThank you for using K-VIEW (\u001B[94m https://github.com/SoldierAb/k-view\u001B[96m)!\u001B[0m\n',
   );
   log(
-    '\u001B[96mFor china, 更多赞助方式(支付宝、微信)请查看如下链接: ' +
-      emoji.get('pray') +
-      '\u001B[0m',
+    '\u001B[96mFor china, 更多赞助方式(支付宝、微信)请查看如下链接: \u001B[0m',
   );
   log('\u001B[96m>\u001B[94m https://github.com/SoldierAb/Front-end-Engineering \u001B[0m\n');
 }
